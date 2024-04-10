5e432cdabae9f5e20bf5bf48f57561e7ef6cbc22
Chandresh Patidar <chandreshpatidar5@gmail.com>
2024-04-10 21:42:23 +0530

    Add api to get geo details

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import React, { useState } from 'react';
 import './app.css';
 import WeatherCard from './components/WeatherCard';
+import { OPEN_WEATHER_BASE_URL } from './config';
 
 function App() {
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
+  const [geocodingData, setGeocodingData] = useState([]);
 
-  const fetchWeatherData = async () => {
+  const fetchGeoDetails = async () => {
     setLoading(true);
 
-    setTimeout(() => {
+    try {
+      const geocodedRes =
+        await fetch(`${OPEN_WEATHER_BASE_URL}geo/1.0/direct?q=${city}&limit=10&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}
+      `);
+
+      const parsedGeoRes = await geocodedRes.json();
+
+      setGeocodingData(parsedGeoRes);
+    } catch (error) {
+      setGeocodingData([]);
+    } finally {
       setLoading(false);
-    }, 1000);
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (city.trim() !== '') {
-      fetchWeatherData();
+      fetchGeoDetails();
     }
   };
 
@@ -33,7 +45,7 @@ function App() {
           value={city}
           onChange={(e) => setCity(e.target.value)}
         />
-        <button type='submit'>Get Weather</button>
+        <button type='submit'>Get City Details</button>
       </form>
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
